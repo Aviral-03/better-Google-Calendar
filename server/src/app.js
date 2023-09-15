ff6b7e6d2eef60058ec5786d71d692708c5350ac
Aviral Bhardwaj <aviral.bhardwaj@mail.utoronto.ca>
2023-09-15 19:19:07 -0400

    Publish Commit
    - Deploy Commit

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const cors = require('cors');
 const loadEnvironment = require('../loadEnvironment.js')
+const path = require('path')
 
 // Routes
 const userRoutes = require('./routes/userRoutes.js');
@@ -14,6 +15,12 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
